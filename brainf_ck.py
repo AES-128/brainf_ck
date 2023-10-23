fd2509c48d730ea472b71525d9a8c047734a11d8
AES-128 <119679392+AES-128@users.noreply.github.com>
2023-10-23 15:24:09 +0100

    oops
    fixed nested loops

--- brainf_ck.py
+++ brainf_ck.py
@@ -1,6 +1,5 @@
 import sys
 
-
 class Brainf_ck:
     def __init__(self, program, DEBUG = False):
         self.program = program
@@ -27,31 +26,38 @@ class Brainf_ck:
             case ",":
                 self.memory[self.data_pointer] = ord(input()[0])
             case "[":
-               self.call_stack.append(self.instruction_pointer)
+                if self.memory[self.data_pointer] == 0:
+                    nesting_level = 1
 
-               if self.memory[self.data_pointer] == 0:
-                    self.call_stack.pop()
-
-                    while self.program[self.instruction_pointer] != "]":
-                       self.instruction_pointer += 1
+                    while nesting_level > 0:
+                        self.instruction_pointer += 1
+                        
+                        if self.program[self.instruction_pointer] == "[":
+                            nesting_level += 1
+                        elif self.program[self.instruction_pointer] == "]":
+                            nesting_level -= 1
+                else:
+                    self.call_stack.append(self.instruction_pointer)
             case "]":
                 if self.memory[self.data_pointer] != 0:
                     self.instruction_pointer = self.call_stack[-1]
+                else:
+                    self.call_stack.pop()
 
         self.instruction_pointer += 1
-
 
     def run_program(self):
         while len(self.program) > self.instruction_pointer:
             if self.DEBUG:
-                print("\033[H")
+                print("\033c")
                 print("\t\t\t------- Memory -------")
                 print(*[f'{hex(num):^4}' for num in range(10)], sep = " | ")
                 print(*[f'{self.memory[idx]:^4}' for idx in range(10)], sep = " | ")
                 print(f"\nData Pointer: {self.data_pointer}\n")
-                print(self.program[:self.instruction_pointer] + f"\033[4m{self.program[self.instruction_pointer]}\033[0m" + self.program[self.instruction_pointer + 1:])
-                input(f"\nInstruction Pointer: {self.instruction_pointer}")    
-            
+                print(self.program[max(0, self.instruction_pointer - 20):self.instruction_pointer] + f"\033[4m{self.program[self.instruction_pointer]}\033[0m" + self.program[self.instruction_pointer + 1:self.instruction_pointer + 20])
+                print(self.call_stack)
+                print(f"\nInstruction Pointer: {self.instruction_pointer}")
+                input()
             self.execute()
 
 bf = Brainf_ck(sys.argv[1])
